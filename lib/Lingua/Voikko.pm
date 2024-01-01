7d6d197ec77b95ac0ccc42bb258ecac8cbc4b6cd
Mikael Rekola <mikael.rekola@gmail.com>
2024-01-02 01:30:25 +0200

    update POD

--- lib/Lingua/Voikko.pm
+++ lib/Lingua/Voikko.pm
@@ -236,45 +236,23 @@ sub tokenize($)
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
-
 =head1 NAME
 
-Lingua::Voikko - Perl extension for blah blah blah
+Lingua::Voikko - Perl extension for Voikko Spellchecker and Lemmatizer
 
 =head1 SYNOPSIS
 
   use Lingua::Voikko;
-  blah blah blah
 
 =head1 DESCRIPTION
-
-Stub documentation for Lingua::Voikko, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
 
 =head2 EXPORT
 
 None by default.
 
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
 =head1 AUTHOR
 
-Mikael Rekola, E<lt>rekola@E<gt>
+Mikael Rekola, E<lt>rekola@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
